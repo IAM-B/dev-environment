800f67fa6a7ab555370247f5e827d015bd2ec68a
IAM-B <salamualyk@pm.me>
2026-04-08 12:00:01 +0100

    Sync configs - 08/04/2026 12:00

--- configs/nvim-lazyvim/lua/config/keymaps.lua
+++ configs/nvim-lazyvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ local map = vim.keymap.set
 
 -- Sauvegarder (Ctrl+s pris par Zellij)
 map({ "n", "i", "v" }, "<leader>w", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- Diff mode
+map("n", "<leader>df", "<cmd>diffthis<cr>", { desc = "Diff this window" })
+map("n", "<leader>dF", "<cmd>diffoff!<cr>", { desc = "Diff off (all windows)" })
--- configs/nvim-lazyvim/lua/config/options.lua
+++ configs/nvim-lazyvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Start Neovim server so external tools (Claude, etc.) can open files here
+local servername = "/tmp/nvim-lazyvim.pipe"
+if vim.fn.filereadable(servername) == 0 then
+  pcall(vim.fn.serverstart, servername)
+end
+
+-- Soft wrap + visual guide for Edge/HTML templates
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("EdgeWrap", { clear = true }),
+  pattern = { "edge", "html" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
